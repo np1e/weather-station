86da5c04a1e89f51ce54a526c7201a007bd41df3
Nick Hafkemeyer <nick.hafkemeyer@gmail.com>
2019-06-17 16:27:00 +0200

    ...

--- src/config.h
+++ src/config.h
@@ -1,3 +1,3 @@
-const String HOST = "192.168.178.23";
-const String SSID = "FRITZ!Box 7490";
-const String PASSWORD = "8TWIM0W88RX5LPO3GFU";
+const char* HOST = "192.168.178.23";
+const char* SSID = "FRITZ!Box 7490";
+const char* PASSWORD = "8TWIM0W88RX5LPO3GFU";
