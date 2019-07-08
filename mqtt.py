2c66f7b1d76eff8e1ff8befee4a7ea0d8cfbd9d1
Nick Hafkemeyer <nick.hafkemeyer@gmail.com>
2019-07-08 15:05:37 +0200

    Added timestamp

--- mqtt.py
+++ mqtt.py
@@ -1,20 +1,21 @@
 import paho.mqtt.client as mqtt
+import datetime
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {str(rc)}.")
     client.subscribe("temperature/#")
-    client.subscribe("test")
+    client.subscribe("humidity")
 
 def on_message(client, userdata, msg):
-    print("Received message:")
-    print(f"{msg.topic}: {msg.payload}")
+    time = datetime.datetime.now()
+    print(f"{time}: {msg.topic}: {str(msg.payload)}")
 
 
 if __name__ == "__main__":
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect("localhost", 1883, 60)
+    client.connect("192.168.178.33", 1883, 60)
     client.loop_forever()
 
 
